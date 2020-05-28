be8390bbb93f4f33351675868084984f8084d79c
Seung-hyun Hwang <32220765+hsh2001@users.noreply.github.com>
2020-05-28 09:03:53 +0900

    Update contents

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const App = () => {
-  return <>Hello world</>;
+  return (
+    <>
+      <h1>안중고 소통GO! 프로젝트는 폐기되었습니다.</h1>
+      <p>
+        교내 사정으로 인해 폐기되었으며 프로젝트의 테스트용 도메인 hsh.kr은
+        개발자 황승현의 개인 소유 도메인입니다. 개발자 황승현에 대해 알고 싶다면{' '}
+        <a href="http://dev.hsh.kr">dev.hsh.kr</a>으로 이동해주세요.
+      </p>
+    </>
+  );
 };
 
 export default App;
